a22dd3b7ea034afb0b0f6638c2c82b3a7fa37266
Jennifer Balangue <jfbalangue@yahoo.com>
2018-07-12 16:29:23 -0400

    Add Github Page to Readme file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # TriviaGame
+
+This game is Javascript based game.  The player will answer multiple choice trivia questions with the 90 second allotted time. The game ends when the time runs out or the player submits their answers.  The game will reveal correct answers in green and wrong answers in red.  There will also be a total of answers correct out of total questions supplied.
+Player is not allowed to select more than one answer.
+
+When game is complete, player has the option to reset the game.
+
+GITHUB PAGE:
+https://balangue1.github.io/TriviaGame/
+
+
+PREREQUISITES:
+Internet Browser
+
+INSTALLING:
+N/A
+
+DEPLOYMENT:
+N/A
+
+BUILT WITH:
+Visual Studio Code
+
+AUTHOR:
+Jennifer Balangue
+
+ACKNOWLEDGEMENTS:
+Coding Bootcamp - Georgia Tech
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,196 +1,196 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   // Create countdown timer for 90 seconds, If timeleft=0 show results
   var timeleft = 90;
-  var downloadTimer = setInterval(function(){
-  timeleft--;
-  document.getElementById("countdowntimer").textContent = timeleft;
-  console.log(timeleft)
-  
-    if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    alert("Time is UP!");
-    showResults();
+  var downloadTimer = setInterval(function () {
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    console.log(timeleft)
 
-  };
-  },1000);
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      alert("Time is UP!");
+      showResults();
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+    };
+  }, 1000);
 
-// Create question bank with answers
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
 
-const myQuestions = [
-  {
-    question: "Which dog breed is noted for it's spotted coat?",
-    answers: {
-      a: "Chihuahua",
-      b: "Dalmation",
-      c: "Poodle"
+  // Create question bank with answers
+
+  const myQuestions = [
+    {
+      question: "Which dog breed is noted for it's spotted coat?",
+      answers: {
+        a: "Chihuahua",
+        b: "Dalmation",
+        c: "Poodle"
+      },
+      correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "What animal's diet is made up almost entirely of eucalypti leaves?",
-    answers: {
-      a: "Panda",
-      b: "Elephant",
-      c: "Koala"
+    {
+      question: "What animal's diet is made up almost entirely of eucalypti leaves?",
+      answers: {
+        a: "Panda",
+        b: "Elephant",
+        c: "Koala"
+      },
+      correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "Who was the first guest star on the Muppet Show?",
-    answers: {
-      a: "Lena Horne",
-      b: "Alice Cooper",
-      c: "John Denver",
+    {
+      question: "Who was the first guest star on the Muppet Show?",
+      answers: {
+        a: "Lena Horne",
+        b: "Alice Cooper",
+        c: "John Denver",
+      },
+      correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "Who is the famous artist who painted Starry Night?",
-    answers: {
-      a: "Vincent van Gogh",
-      b: "Donald Trump",
-      c: "DaVinci"
+    {
+      question: "Who is the famous artist who painted Starry Night?",
+      answers: {
+        a: "Vincent van Gogh",
+        b: "Donald Trump",
+        c: "DaVinci"
+      },
+      correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "Who directed The Passion of the Christ?",
-    answers: {
-      a: "Stan Lee",
-      b: "Mel Gibson",
-      c: "Robert Redford"
+    {
+      question: "Who directed The Passion of the Christ?",
+      answers: {
+        a: "Stan Lee",
+        b: "Mel Gibson",
+        c: "Robert Redford"
+      },
+      correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "The dingo is a type of feral dog native to what country?",
-    answers: {
-      a: "Australia",
-      b: "Asia",
-      c: "North America",
+    {
+      question: "The dingo is a type of feral dog native to what country?",
+      answers: {
+        a: "Australia",
+        b: "Asia",
+        c: "North America",
+      },
+      correctAnswer: "a"
     },
-    correctAnswer: "a"
-  },
-  {
-    question: "Stratus, Cirrus and Cumulus are types of what?",
-    answers: {
-      a: "Sand",
-      b: "Fossils",
-      c: "Clouds",
+    {
+      question: "Stratus, Cirrus and Cumulus are types of what?",
+      answers: {
+        a: "Sand",
+        b: "Fossils",
+        c: "Clouds",
+      },
+      correctAnswer: "c"
     },
-    correctAnswer: "c"
-  },
-  {
-    question: "What is the world's largest ocean?",
-    answers: {
-      a: "Atlantic",
-      b: "Pacific",
-      c: "Indian",
+    {
+      question: "What is the world's largest ocean?",
+      answers: {
+        a: "Atlantic",
+        b: "Pacific",
+        c: "Indian",
+      },
+      correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "Lox, often served on a bagel, is a fillet of brined what?",
-    answers: {
-      a: "Squid",
-      b: "Salmon",
-      c: "Shrimp",
+    {
+      question: "Lox, often served on a bagel, is a fillet of brined what?",
+      answers: {
+        a: "Squid",
+        b: "Salmon",
+        c: "Shrimp",
+      },
+      correctAnswer: "b"
     },
-    correctAnswer: "b"
-  },
-  {
-    question: "What is the name of the Disney cartoon character that is the girlfriend of Donald Duck?",
-    answers: {
-      a: "Maria",
-      b: "Lucy",
-      c: "Daisy",
+    {
+      question: "What is the name of the Disney cartoon character that is the girlfriend of Donald Duck?",
+      answers: {
+        a: "Maria",
+        b: "Lucy",
+        c: "Daisy",
+      },
+      correctAnswer: "c"
     },
-    correctAnswer: "c"
-  }  
-];
+  ];
 
 
-function buildQuiz(){
-  // we'll need a place to store the HTML output
-  const output = [];
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
+    const output = [];
 
-  
-  // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
 
-      // we'll want to store the list of answer choices
-      const answers = [];
-      
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
 
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
+        // we'll want to store the list of answer choices
+        const answers = [];
+
+        // and for each available answer...
+        for (letter in currentQuestion.answers) {
+
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
           </label>`
-        );
-      }
+          );
+        }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
+        // add this question and its answers to the output
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join('')} </div>`
-      );
-    
-});
+        );
 
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
-};
+      });
 
-// display quiz right away
-buildQuiz();
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+  };
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+  // display quiz right away
+  buildQuiz();
+
+  // on submit, show results
+  submitButton.addEventListener('click', showResults);
 
 
 
-function showResults(){
+  function showResults() {
 
     // gather answer containers from our quiz
     const answerContainers = quizContainer.querySelectorAll('.answers');
-  
+
     // keep track of user's answers
     let numCorrect = 0;
-  
+
     // for each question...
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
       // find selected answer
       const answerContainer = answerContainers[questionNumber];
-      const selector = 'input[name=question'+questionNumber+']:checked';
+      const selector = 'input[name=question' + questionNumber + ']:checked';
       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
+
       // if answer is correct
-      if(userAnswer===currentQuestion.correctAnswer){
+      if (userAnswer === currentQuestion.correctAnswer) {
         // add to the number of correct answers
         numCorrect++;
-  
+
         // color the answers green
         answerContainers[questionNumber].style.color = 'green';
       }
       // if answer is wrong or blank
-      else{
+      else {
         // color the answers red
         answerContainers[questionNumber].style.color = 'red';
       }
     });
-  
+
     // show number of correct answers out of total
     resultsContainer.innerHTML = numCorrect + ' correct out of ' + myQuestions.length;
 
